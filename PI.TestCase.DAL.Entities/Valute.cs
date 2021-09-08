a87d85698e0eae020a3724b1f15e713b62a561ae
Vladimir Berdnikov <gazetter@mail.ru>
2021-09-08 22:47:09 +0700

    Накидал каркас трехслойки и сущности валюты

---
+++ PI.TestCase.DAL.Entities/Valute.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace PI.TestCase.DAL.Entities
+{
+    public class Valute
+    {
+        public string Id { get; set; }
+        public int NumCode { get; set; }
+        public string CharCode { get; set; }
+        public int Nominal { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public decimal Prevous { get; set; }
+
+
+
+
+    }
+}
