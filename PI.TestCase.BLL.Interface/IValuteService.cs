14def07cf7432807a19ddfad142e2d9198948748
Vladimir Berdnikov <gazetter@mail.ru>
2021-09-09 17:16:23 +0700

    Накидал интерфейс бизнес-логики, надо обновить студию доставить UWP и обновить win10 до актуальной версии для которой есть SDK

---
+++ PI.TestCase.BLL.Interface/IValuteService.cs
@@ -0,0 +1,22 @@
+﻿using PI.TestCase.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI.TestCase.BLL.Interface
+{
+    public interface IValuteService
+    {
+        public IEnumerable<Valute> GetAll();
+        public Valute GetById(string id);
+        public IEnumerable<Valute> GetByName(string name);
+        public Valute GetByCharCode(string charCode);
+        public void Add(Valute newValute);
+        public void Edit(string id);
+        public void Delete(string id);        
+
+
+    }
+}
